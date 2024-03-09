ee6fa50c4315390d46c182b33fb7b186b0c66525
maria.suarez <aplicacionesinternas1@helisa.com>
2024-03-09 11:05:17 -0500

    Update dependencies and add images to the about page.

--- src/app/impact/impact.component.ts
+++ src/app/impact/impact.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-impact',
   templateUrl: './impact.component.html',
   styleUrls: ['./impact.component.css']
 })
-export class ImpactComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class ImpactComponent {}
