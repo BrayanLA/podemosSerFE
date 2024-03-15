0e93189a77227907219af7cc50bdd20ed6f45c9d
Paola Beltran <pbeltran@activeit.cl>
2024-03-15 11:48:21 -0500

    Timeline and homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,4 +13,26 @@ export class HomeComponent implements OnInit {
 
   }
 
+  ngAfterViewInit(): void {
+    this.slidesPlugin();
+  }
+  slidesPlugin(activeSlide: number = 2): void {
+    const slides = document.querySelectorAll(".slide");
+
+    (slides[activeSlide] as HTMLElement).classList.add("active");
+
+    for (const slide of Array.from(slides)) {
+      slide.addEventListener("click", () => {
+        this.clearActiveClasses(slides);
+        slide.classList.add("active");
+      });
+    }
+  }
+
+  clearActiveClasses(slides: NodeListOf<Element>): void {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  }
+
 }
