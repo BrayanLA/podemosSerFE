e626354c8d0fc8504f09c1ebd45f1dc67b0767f4
BrayanLA <brayan.david0@hotmail.com>
2025-02-27 19:32:04 -0500

    V1.0

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -9,29 +9,52 @@ import { Observable } from 'rxjs';
 export class FooterComponent implements OnInit {
 
   email: string = '';
-  private apiUrl = 'http://podemosser.com/enviar-correo';
+  message: string | null = null;
+  tooltipClass: string = '';  // Cambiamos de tooltipType a tooltipClass
+  private apiUrl = 'https://podemosser.com/enviar-correo';
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   subscribe() {
     if (this.email) {
       const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
       const body = { email: this.email, date: new Date().toISOString() };
-      this.http.post('https://podemosser.com/enviar-correo', body, { headers: headers }).subscribe(
+
+      this.http.post(this.apiUrl, body, { headers: headers, observe: 'response', responseType: 'text' }).subscribe(
         response => {
-          console.log('Subscription successful', response);
-          // Aquí puedes manejar una respuesta exitosa, como mostrar un mensaje al usuario.
+          console.log("Respuesta recibida:", response); // Verifica toda la respuesta
+          if (response.status === 201) {
+            this.message = '¡Gracias por suscribirte! Pronto recibirás nuestras novedades.';
+            this.tooltipClass = 'show-tooltip success';
+          } else {
+            console.log("Código de estado inesperado:", response.status); // Manejo de códigos no esperados
+          }
         },
         error => {
-          console.error('Subscription failed', error);
-          // Aquí puedes manejar el error, como mostrar un mensaje al usuario.
+          console.log("Error recibido:", error); // Verifica el error completo
+          if (error.status === 409) { // Maneja el conflicto aquí
+            this.message = 'Parece que ya te has suscrito antes. ¡Gracias por formar parte de nuestra comunidad!';
+            this.tooltipClass = 'show-tooltip warning';
+          } else {
+            this.message = 'Hubo un problema con tu suscripción. Por favor, inténtalo de nuevo más tarde.';
+            this.tooltipClass = 'show-tooltip error';
+          }
         }
       );
     } else {
-      console.error('Email is required');
+      console.log("No se ingresó un correo electrónico");
+      this.message = 'Por favor, ingresa un correo electrónico válido.';
+      this.tooltipClass = 'show-tooltip warning';
     }
+
+    setTimeout(() => {
+      this.message = null;
+      this.tooltipClass = '';
+    }, 5000);
   }
+
 }
+
+
--- src/app/impact/impact.component.ts
+++ src/app/impact/impact.component.ts
@@ -5,4 +5,49 @@ import { Component } from '@angular/core';
   templateUrl: './impact.component.html',
   styleUrls: ['./impact.component.css']
 })
-export class ImpactComponent {}
+export class ImpactComponent {
+
+  imagenesMarzo2021:String[] = ["../../assets/2021-comprimidas/03 Marzo_Jóvenes Iberoamericanos.jpeg","../../assets/2021-comprimidas/03 Marzo_Universidad del Sinú (Montería).jpg"]
+
+  imagenesAgosto2022: string[] = [
+    "../../assets/img/Carrousell/2022-comprimidas/08 Agosto_Marea Púrpura (Bogotá).jpg",
+    "../../assets/2022-comprimidas/08 Agosto_Marea Púrpura (Barranquilla).jpg",
+    "../../assets/2022-comprimidas/08 Agosto_Marea Púrpura (Buenaventura).jpg",
+    "../../assets/2022-comprimidas/08 Agosto_Marea Púrpura (Cucuta).jpg"
+  ];
+
+  imagenesNoviembre2022: string[] = [
+    "../../assets/img/Carrousell/2022-comprimidas/11 Noviembre_Colegio Mayor San bartolomé (2).jpeg",
+    "../../assets/img/Carrousell/2022-comprimidas/11 Noviembre_Colegio Mayor San bartolomé (4).jpeg",
+    "../../assets/img/Carrousell/2022-comprimidas/11 Noviembre_Colegio Mayor San bartolomé.jpeg",
+    "../../assets/img/Carrousell/2022-comprimidas/11 Noviembre_Mentorías KAS.jpg",
+    "../../assets/img/Carrousell/2022-comprimidas/11 Noviembre_Premio Dejar Huella (1).jpeg",
+    "../../assets/img/Carrousell/2022-comprimidas/11 Noviembre_Premio Dejar Huella (2).jpeg"
+  ];
+
+  abril2023: string[] = [
+    "../../assets/img/Carrousell/2023-comprimidas/04 Abril_Marea Púrpura (Bucaramanga).jpg",
+    "../../assets/img/Carrousell/2023-comprimidas/04 Abril_Acción Interna.jpeg",
+    "../../assets/img/Carrousell/2023-comprimidas/04 Abril_Marea Púrpura (Bogotá).jpg",
+    "../../assets/img/Carrousell/2023-comprimidas/04 Abril_Marea Púrpura (Medellín).jpg",
+    "../../assets/img/Carrousell/2023-comprimidas/04 Abril_Marea Púrpura (Tumaco).jpg",
+    "../../assets/img/Carrousell/2023-comprimidas/04 Abril_Ruta País (Mesa técnica - Mocoa 2).jpg",
+    "../../assets/img/Carrousell/2023-comprimidas/04 Abril_Ruta País (Mesa técnica - Mocoa).jpg",
+    "../../assets/img/Carrousell/2023-comprimidas/04 Abril_Ruta País (Socialización - Mocoa).jpg"
+  ];
+  septiembre2023: string[] = [
+    "../../assets/img/Carrousell/2023-comprimidas/09 Septiembre_Mesa técnica Villavicencio.jpg",
+    "../../assets/img/Carrousell/2023-comprimidas/09 Septiembre_Socialización Villavicencio.jpg",
+  ];
+  marzo2024: string[] = [
+    "../../assets/img/Carrousell/2024-comprimidas/8 marzo_socialización Universidad de Medellín-min.jpeg",
+    "../../assets/img/Carrousell/2024-comprimidas/8 marzo_socialización Universidad de Medellín (2)-min.jpeg",
+    "../../assets/img/Carrousell/2023-comprimidas/12 Diciembre_Vista Hermosa (1).jpg"
+  ];
+  imagenesJunio2024: string[] = [
+    "../../assets/img/Carrousell/2024-comprimidas/10 junio_socialización Soy Líder-min.jpeg",
+    "../../assets/img/Carrousell/2024-comprimidas/10 junio_socialización Soy Líder(2)-min.jpeg",
+    "../../assets/img/Carrousell/2024-comprimidas/13 junio_ socialización obra de teatro Amarte Así (2)-min.jpeg"
+  ];
+  constructor(){}
+}
