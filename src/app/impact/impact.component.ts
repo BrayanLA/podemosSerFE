abf9ea8d0064396e9423ec1b25d6b67674823a7d
BrayanLA <brayan.david0@hotmail.com>
2025-03-18 20:22:42 -0500

    Ajustes linea de tiempo

--- src/app/impact/impact.component.ts
+++ src/app/impact/impact.component.ts
@@ -7,15 +7,48 @@ import { Component } from '@angular/core';
 })
 export class ImpactComponent {
 
-  
+  imagenesFebrero2021: string[] = [
+    "../../assets/img/Carrousell/2021-comprimidas/01 Enero_Lanzamiento.jpg"
+  ];
 
-  imagenesMarzo2021:String[] = ["../../assets/2021-comprimidas/03 Marzo_Jóvenes Iberoamericanos.jpeg","../../assets/2021-comprimidas/03 Marzo_Universidad del Sinú (Montería).jpg"]
-
+  imagenesMarzo2021: string[] = [
+    "../../assets/2021-comprimidas/03 Marzo_Jóvenes Iberoamericanos.jpeg",
+    "../../assets/2021-comprimidas/03 Marzo_Universidad del Sinú (Montería).jpg"
+  ];
+  imagenesJulio2021: string[] = [
+    "../../assets/img/Carrousell/2021-comprimidas/07 Julio_Socialización Medellín.jpg"
+  ];
+  imagenesAgosto2021: string[] = [
+    "../../assets/img/Carrousell/2021-comprimidas/08 Agosto_Ibagué (mesa técnica).jpg",
+    "../../assets/2021-comprimidas/08 Agosto_Ibagué.jpg",
+    "../../assets/2021-comprimidas/08 Agosto_Ibagué(3).jpg"
+  ];
+  imagenesSeptiembre2021: string[] = [
+    "../../assets/img/Carrousell/2021-comprimidas/09 Septiembre_Manizales (2).jpg",
+    "../../assets/2021-comprimidas/09 Septiembre_Manizales.jpg",
+    "../../assets/2021-comprimidas/09 Septiembre_Manizales (3).jpg",
+    "../../assets/2021-comprimidas/09 Septiembre_Manizales (4).jpg"
+  ];
+  imagenesOctubre2021: string[] = [
+    "../../assets/img/Carrousell/2021-comprimidas/10 Octubre Socialización Cali (2).jpeg",
+    "../../assets/2021-comprimidas/10 Octubre_En Femenino.jpg",
+    "../../assets/2021-comprimidas/10 Octubre_Socialización Cali (2).jpg",
+    "../../assets/2021-comprimidas/10 Octubre_Socialización Cali - Biblioteca.jpg",
+    "../../assets/2021-comprimidas/10 Octubre_Socialización Cali.jpg"
+  ];
+  imagenesMarzo2022: string[] = [
+    "../../assets/img/Carrousell/2022-comprimidas/03 Marzo_Socialización Bogotá (1).jpg",
+    "../../assets/2022-comprimidas/03 Marzo_Socialización Bogotá (2).jpg",
+    "../../assets/2022-comprimidas/03 Marzo_Socialización Bogotá.jpg"
+  ];
   imagenesAgosto2022: string[] = [
     "../../assets/img/Carrousell/2022-comprimidas/08 Agosto_Marea Púrpura (Bogotá).jpg",
     "../../assets/2022-comprimidas/08 Agosto_Marea Púrpura (Barranquilla).jpg",
     "../../assets/2022-comprimidas/08 Agosto_Marea Púrpura (Buenaventura).jpg",
     "../../assets/2022-comprimidas/08 Agosto_Marea Púrpura (Cucuta).jpg"
+  ];
+  imagenesSeptiembre2022: string[] = [
+    "../../assets/img/Carrousell/2022-comprimidas/09 Septiembre_Lanzamiento.jpg"
   ];
 
   imagenesNoviembre2022: string[] = [
@@ -26,8 +59,11 @@ export class ImpactComponent {
     "../../assets/img/Carrousell/2022-comprimidas/11 Noviembre_Premio Dejar Huella (1).jpeg",
     "../../assets/img/Carrousell/2022-comprimidas/11 Noviembre_Premio Dejar Huella (2).jpeg"
   ];
+  imagenesMarzo2023: string[] = [
+    "../../assets/2023-comprimidas/2023.png"
+  ];
 
-  abril2023: string[] = [
+  imagenesAbril2023: string[] = [
     "../../assets/img/Carrousell/2023-comprimidas/04 Abril_Marea Púrpura (Bucaramanga).jpg",
     "../../assets/img/Carrousell/2023-comprimidas/04 Abril_Acción Interna.jpeg",
     "../../assets/img/Carrousell/2023-comprimidas/04 Abril_Marea Púrpura (Bogotá).jpg",
@@ -37,14 +73,39 @@ export class ImpactComponent {
     "../../assets/img/Carrousell/2023-comprimidas/04 Abril_Ruta País (Mesa técnica - Mocoa).jpg",
     "../../assets/img/Carrousell/2023-comprimidas/04 Abril_Ruta País (Socialización - Mocoa).jpg"
   ];
-  septiembre2023: string[] = [
+  imagenesJunio2023: string[] = [
+    "../../assets/img/Carrousell/2023-comprimidas/06 Junio_Mentorías Valle del Cauca.jpg",
+    "../../assets/img/Carrousell/2023-comprimidas/06 Junio_Mentorías Valle del Cauca (2).jpg"
+  ];
+  imagenesJulio2023: string[] = [
+    "../../assets/img/Carrousell/2023-comprimidas/07 Julio_Entrevista doble radio G.jpg"
+  ];
+  imagenesSeptiembre2023: string[] = [
     "../../assets/img/Carrousell/2023-comprimidas/09 Septiembre_Mesa técnica Villavicencio.jpg",
     "../../assets/img/Carrousell/2023-comprimidas/09 Septiembre_Socialización Villavicencio.jpg",
   ];
-  marzo2024: string[] = [
+  imagenesOctubre2023: string[] = [
+    "../../assets/img/Carrousell/2023-comprimidas/10 Octubre_Jornada juvenil Valledupar.jpg"
+  ];
+  imagenesNoviembre2023: string[] = [
+    "../../assets/img/Carrousell/2023-comprimidas/11 Noviembre_ELIGEP 2050.jpg"
+  ];
+  imagenesDiciembre2023: string[] = [
+    "../../assets/img/Carrousell/2023-comprimidas/12 Diciembre_Vista Hermosa.jpg",
+    "../../assets/img/Carrousell/2023-comprimidas/12 Diciembre_Vista Hermosa (1).jpg"
+  ];
+  imagenesMarzo2024: string[] = [
     "../../assets/img/Carrousell/2024-comprimidas/8 marzo_socialización Universidad de Medellín-min.jpeg",
     "../../assets/img/Carrousell/2024-comprimidas/8 marzo_socialización Universidad de Medellín (2)-min.jpeg",
     "../../assets/img/Carrousell/2023-comprimidas/12 Diciembre_Vista Hermosa (1).jpg"
+  ];
+  imagenesAbril2024: string[] = [
+    "../../assets/img/Carrousell/2024-comprimidas/29 abril_programa de mentorías (2) Buga-min.jpeg",
+    "../../assets/img/Carrousell/2024-comprimidas/29 abril_programa de mentorías (3)Buga-min.jpeg"
+  ];
+  imagenesMayo2024: string[] = [
+    "../../assets/2024-comprimidas/22 mayo_socialización KAS PPI (2)-min.jpeg",
+    "../../assets/2024-comprimidas/30 mayo_socialización Universidad de Medellín-min.jpeg"
   ];
   imagenesJunio2024: string[] = [
     "../../assets/img/Carrousell/2024-comprimidas/10 junio_socialización Soy Líder-min.jpeg",
@@ -88,5 +149,178 @@ export class ImpactComponent {
     "../../assets/2025-comprimidas/12 marzo_Podemos Ser en Mastercard.jpg",
     "../../assets/2025-comprimidas/12 marzo_VII Festival Literario de Mujeres.jpg",
   ];
+  eventos2021 = [
+    {
+      id: 'febrero2021',
+      title: 'Febrero 2021',
+      images: this.imagenesFebrero2021
+    },
+    {
+      id: 'marzo2021',
+      title: 'Marzo 2021',
+      images: this.imagenesMarzo2021
+    },
+    {
+      id: 'julio2021',
+      title: 'Julio 2021',
+      images: this.imagenesJulio2021
+    },
+    {
+      id: 'agosto2021',
+      title: 'Agosto 2021',
+      images: this.imagenesAgosto2021
+    },
+    {
+      id: 'septiembre2021',
+      title: 'Septiembre 2021',
+      images: this.imagenesSeptiembre2021
+    },
+    {
+      id: 'octubre2021',
+      title: 'Octubre 2021',
+      images: this.imagenesOctubre2021
+    }
+  ];
+
+  eventos2022 = [
+    {
+      id: 'marzo2022',
+      title: 'Marzo 2022',
+      images: this.imagenesMarzo2022
+    },
+    {
+      id: 'agosto2022',
+      title: 'Agosto 2022',
+      images: this.imagenesAgosto2022
+    },
+    {
+      id: 'septiembre2022',
+      title: 'Septiembre 2022',
+      images: this.imagenesSeptiembre2022
+    },
+    {
+      id: 'noviembre2022',
+      title: 'Noviembre 2022',
+      images: this.imagenesNoviembre2022
+    }
+  ];
+
+  eventos2023 = [
+    {
+      id: 'marzo2023',
+      title: 'Marzo 2023',
+      images: this.imagenesMarzo2023
+    },
+    {
+      id: 'abril2023',
+      title: 'Abril 2023',
+      images: this.imagenesAbril2023
+    },
+    {
+      id: 'junio2023',
+      title: 'Junio 2023',
+      images: this.imagenesJunio2023
+    },
+    {
+      id: 'junio2023',
+      title: 'Junio 2023',
+      images: this.imagenesJulio2023
+    },
+    {
+      id: 'septiembre2023',
+      title: 'Septiembre 2023',
+      images: this.imagenesSeptiembre2023
+    },
+    {
+      id: 'octubre2023',
+      title: 'Octubre 2023',
+      images: this.imagenesOctubre2023
+    },
+    {
+      id: 'noviembre2023',
+      title: 'Noviembre 2023',
+      images: this.imagenesNoviembre2023
+    },
+    {
+      id: 'diciembre2023',
+      title: 'Diciembre 2023',
+      images: this.imagenesDiciembre2023
+    }
+  ];
+
+  // Para 2024, 2025, y así sucesivamente...
+  eventos2024 = [
+    {
+      id: 'marzo2024',
+      title: 'Marzo 2024',
+      images: this.imagenesMarzo2024
+    },
+    {
+      id: 'abril2024',
+      title: 'Abril 2024',
+      images: this.imagenesAbril2024
+    },
+    {
+      id: 'mayo2024',
+      title: 'Mayo 2024',
+      images: this.imagenesMayo2024
+    },
+    {
+      id: 'junio2024',
+      title: 'Junio 2024',
+      images: this.imagenesJunio2024
+    },
+    {
+      id: 'julio2024',
+      title: 'Julio 2024',
+      images: this.imagenesJulio2024
+    },
+    {
+      id: 'septiembre2024',
+      title: 'Septiembre 2024',
+      images: this.imagenesSeptiembre2024
+    },
+    {
+      id: 'octubre2024',
+      title: 'Octubre 2024',
+      images: this.imagenesOctubre2024
+    },
+    {
+      id: 'noviembre2024',
+      title: 'Noviembre 2024',
+      images: this.imagenesNoviembre2024
+    },
+    {
+      id: 'diciembre2024',
+      title: 'Diciembre 2024',
+      images: this.imagenesDiciembre2024
+    },
+  ];
+
+  eventos2025 = [
+    {
+      id: 'febrero2025',
+      title: 'Febrero 2025',
+      images: this.imagenesFebrero2025
+    },
+    {
+      id: 'marzo2025',
+      title: 'Marzo 2025',
+      images: this.imagenesMarzo2025
+    }
+  ];
+  titleColors: string[] = ['#df7c53', '#0c8cab', '#4f2352'];
+
+  // Variable que almacenará el color seleccionado al abrir el modal
+  selectedTitleColor = '#df7c53';
+  selectedEvent: { id: string; title: string; images: string[] } | null = null;
+
+  openModal(evento: { id: string; title: string; images: string[] }): void {
+    this.selectedEvent = evento;
+
+    // Escoge un color aleatorio del arreglo
+    const randomIndex = Math.floor(Math.random() * this.titleColors.length);
+    this.selectedTitleColor = this.titleColors[randomIndex];
+  }
   constructor(){}
 }
